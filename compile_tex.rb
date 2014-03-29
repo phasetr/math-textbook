0ea56a7e3377257358f1ba6343f8310278885af6
phasetr <phasetr@gmail.com>
2014-03-29 17:48:31 +0900

    Add references.

--- compile_tex.rb
+++ compile_tex.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/ruby
 orig_file = ARGV[0]
 file_basename = File.basename(orig_file, ".tex")
-`platex #{file_basename}`
-`pbibtex #{file_basename}`
-`platex #{file_basename}`
-`platex #{file_basename}`
-`dvipdfmx #{file_basename}.dvi`
+puts `platex #{file_basename}`
+puts `mendex #{file_basename}`
+puts `pbibtex #{file_basename}`
+puts `platex #{file_basename}`
+puts `platex #{file_basename}`
+puts `dvipdfmx #{file_basename}.dvi`

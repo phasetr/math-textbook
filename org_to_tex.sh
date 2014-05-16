26e74782a6c2701e8c1143c452c8be727eec2b8f
esotericaone <esotericaone@gmail.com>
2014-05-16 23:51:35 +0900

    support other Emacs path

--- org_to_tex.sh
+++ org_to_tex.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-/Applications/Emacs.app/Contents/MacOS/Emacs -nw \
-    --batch \
-    --load=org-sett.el \
-    --visit=math_textbook.org \
-    --funcall org-export-as-latex
+
+EMACS_MACOSX="/Applications/Emacs.app/Contents/MacOS/Emacs"
+EMACS_OPTIONS="-nw --batch --load=org-sett.el --visit=math_textbook.org --funcall org-export-as-latex"
+
+EMACS_WHICH=`which emacs`
+if [ $? -eq 0 ]; then
+    EMACS_PATH="$EMACS_WHICH"
+elif [ -f $EMACS_MACOSX ]; then
+    EMACS_PATH=$EMACS_MACOSX
+fi
+
+$EMACS_PATH $EMACS_OPTIONS
+
